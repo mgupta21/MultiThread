639b8a2d1dd995657a8e26e67a4833e5f31572d9
sabu <sabu@mail.com>
2015-08-10 23:04:08 -0500

    Initializing maven project

---
+++ MultiThread/src/main/java/com/java/multiThread/App.java
@@ -0,0 +1,13 @@
+package com.java.multiThread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
